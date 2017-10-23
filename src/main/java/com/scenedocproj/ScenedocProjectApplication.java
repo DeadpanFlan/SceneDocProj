5a8f385d277fa0ee7970270a16679c426e1989dc
Jason Flanagan <jtflanag@gmail.com>
2017-10-23 00:28:42 -0400

    First Commit, Spring Boot App Starter

---
+++ src/main/java/com/scenedocproj/ScenedocProjectApplication.java
@@ -0,0 +1,12 @@
+package com.scenedocproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenedocProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenedocProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/scenedocproj/ScenedocProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scenedocproj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScenedocProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
